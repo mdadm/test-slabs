80ae18c6f7339584bc6f0360b3f33d4e1b536697
M Dadm <mdadm.prog@gmail.com>
2021-05-24 19:24:55 +0500

    feat: Expand/collapse button replaced with an icon

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,9 @@ import { ProductCard } from "./components/ProductCard";
 import { SubProduct } from "./components/SubProduct";
 import { useActualProductList } from "./hooks/useActualProductList";
 import { ErrorMessage } from "./components/ErrorMessage";
+import { IconButton } from "@material-ui/core";
+import ArrowLeft from '@material-ui/icons/ArrowLeft';
+import ArrowDropDown from '@material-ui/icons/ArrowDropDown';
 
 function App() {
   const [activeProductIndex, setActiveProductIndex] = useState(-1);
@@ -22,28 +25,30 @@ function App() {
         <h1>Products</h1>
       </div>
       {productsList.length ? productsList.map((item, key) => (
-        <div className="products-panel" key={`products-panel-${key}`}>
-          <div key={key}
-               className={`products${key === activeProductIndex ? ' active' : ''}`}
-               data-index={key}
-          >
-            <ProductCard product={item}/>
-            <button className="button" onClick={() => openTab(key)}>
-              {key !== activeProductIndex ? 'expand' : 'collapse'}
-            </button>
+          <div className="products-panel" key={`products-panel-${key}`}>
+            <div key={key}
+                 className={`products${key === activeProductIndex ? ' active' : ''}`}
+                 data-index={key}
+            >
+              <ProductCard product={item}/>
+              <div>
+                <IconButton aria-label="expand" onClick={() => openTab(key)} >
+                  {key !== activeProductIndex ? <ArrowLeft fontSize="large" /> : <ArrowDropDown fontSize="large" />}
+                </IconButton>
+              </div>
+            </div>
+            <div>
+              {activeProductIndex === key && (
+                <SubProduct {...productsList[activeProductIndex]} />)}
+            </div>
+            <hr/>
           </div>
-          <div>
-            {activeProductIndex === key && (
-              <SubProduct {...productsList[activeProductIndex]} />)}
-          </div>
-          <hr/>
-        </div>
-      )) :
+        )) :
         <ErrorMessage
           message="Something went wrong!"
           description="Seems like you forgot set up an url to .env file or
           some errors occurred when receiving data from the server"
-        /> }
+        />}
     </div>
   )
 }
